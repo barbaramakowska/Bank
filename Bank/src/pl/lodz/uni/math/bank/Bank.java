281dcd237ed347ea63c440384a73fadc08c2a62d
Basia <Basia@Dell>
2017-04-01 09:23:32 +0200

    bank-singleton

--- Bank/src/pl/lodz/uni/math/bank/Bank.java
+++ Bank/src/pl/lodz/uni/math/bank/Bank.java
@@ -1,10 +1,14 @@
 package pl.lodz.uni.math.bank;
 
 public class Bank extends Address {
+	private static Bank instance = null;
 	String name;
-	
-	
-	
-	
 
+	private Bank() {
+	}
+
+	public static Bank getInstance() {
+		System.out.println("yes getInstance");
+		return instance;
+	}
 }
